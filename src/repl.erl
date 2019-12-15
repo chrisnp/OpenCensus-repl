1a9ed4c94c8859031ea56c95c3d46271c6479878
Invigo <invigo.devel@gmail.com>
2019-12-15 11:31:04 +0200

    Update repl.erl

--- src/repl.erl
+++ src/repl.erl
@@ -13,10 +13,20 @@ run() ->
 %% Auxiliary functions
 %%====================================================================
 read_eval_process() ->
+    ocp:with_tags(#{method => "repl"}),
+
     Line = io:get_line("> "),
     Out = process_line(Line),
     io:format("< ~s~n~n", [Out]),
     read_eval_process().
 
+read_line() ->
+    io:get_line("> ").
+
 process_line(Line) ->
     string:uppercase(Line).
+
+measures() ->
+    oc_stat_measure:new('repl/latency', "The latency in millisecs per REPL loop", millisecond),
+    oc_stat_measure:new('repl/errors', "The number of errors encountered", none),
+    oc_stat_measure:new('repl/line_lengths', "The distribution of line lengths", bytes).
