bd384a077cda28ba531b3eacc92e916dbaaa6b8e
Invigo <invigo.devel@gmail.com>
2019-12-16 22:57:35 +0200

    Update repl.erl

--- src/repl.erl
+++ src/repl.erl
@@ -32,8 +32,30 @@ process_line(Line) ->
     ocp:record('repl/line_length', erlang:iolist_size(Line)),
     Upper.
 
-
 measures() ->
     oc_stat_measure:new('repl/latency', "The latency in millisecs per REPL loop", millisecond),
     oc_stat_measure:new('repl/errors', "The number of errors encountered", none),
     oc_stat_measure:new('repl/line_lengths', "The distribution of line lengths", bytes).
+
+views() -> 
+    Views = [#{name => 'demo/latency',
+               description => "The distribution of latencies",
+               tags => [method],
+               measure => 'repl/latency',
+               aggregation => latency_distribution()},
+             #{name => 'demo/lines_in',
+               description => "The number of lines from standard input",
+               tags => [method],
+               measure => 'repl/line_length',
+               aggregation => oc_stat_aggregation_count},
+             #{name => 'demo/errors',
+               description => "The number of errors",
+               tags => [],
+               measure => 'repl/errors',
+               aggregation => oc_stat_aggregation_count},
+             #{name => 'demo/line_length',
+               description => "Groups the length of keys in buckets",
+               tags => [method],
+               measure => 'repl/line_length',
+               aggregation => size_distribution()}],
+    [oc_stat_view:subscribe(V) || V <- Views].
