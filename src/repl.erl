ed1a5760c560b589db57df45b9502b8f3799fcd5
Invigo <invigo.devel@gmail.com>
2019-12-20 00:17:05 +0200

    Update repl.erl

--- src/repl.erl
+++ src/repl.erl
@@ -18,8 +18,7 @@ run() ->
 %%====================================================================
 read_eval_process() ->
     ocp:with_tags(#{method => "repl"}),
-
-    Line = io:get_line("> "),
+    Line = read_line(), %io:get_line("|> "),
     Out = process_line(Line),
     io:format("< ~s~n~n", [Out]),
     read_eval_process().
@@ -35,6 +34,7 @@ process_line(Line) ->
                                                 native, millisecond)),
     ocp:record('repl/line_length', erlang:iolist_size(Line)),
     Upper.
+    % read_line().
 
 measures() ->
     oc_stat_measure:new('repl/latency', "The latency in millisecs per REPL loop", millisecond),
